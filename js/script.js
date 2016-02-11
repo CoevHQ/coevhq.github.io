ce32749a5a71221bb39d63e2863512d22ac1d8e8
CoEvolution Laboratories <jenerestain@gmail.com>
2016-02-11 22:02:31 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 var start = function(){
   var name = ""
-  document.getElementById('response').innerHTML ="<samp>" + "<p class='text-muted'>*You wake up in a strange mystical place and a tall thin man stands in front of you*</p>'Hello and Welcome to Rifthalen <br> You have been summoned here by the God Queen.'" + "</samp>";
+  document.getElementById('response').innerHTML ="<samp>" + "<p class='text-muted'>*You wake up in a strange mystical place and a tall thin man stands in front of you*</p>'Hello and Welcome to Rifthalen.' <br> 'You have been summoned here by the God Queen.'" + "</samp>";
     }
