45146b5ba75b862d32d1e06f77ad3b70247ee152
CoEvolution Laboratories <jenerestain@gmail.com>
2016-02-11 21:46:55 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,8 +4,7 @@ var start = function(){
   //Check For Answers
   var checkAns = setInterval(function(){ 
     if(name==""){
-      document.getElementById('submitin').onclick = getInfo()
-      name = getInfo();
+      name = document.getElementById('submitin').onclick = getInfo()
       String(name)
     }
     else{
