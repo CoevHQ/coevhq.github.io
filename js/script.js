2b188aa1dec13b14a6dc4ae21a2297c6a946e168
CoEvolution Laboratories <jenerestain@gmail.com>
2016-02-11 21:25:28 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ var start = function(){
     if(name==""){
       name = getInfo();
       String(name)
-      document.getElementById('response').innerHTML ="<samp>" + "<p class="text-muted">*You wake up in a strange mystical place and a tall thin man stands in front of you*</p><br>'What is your name?'" + "</samp>";
+      document.getElementById('response').innerHTML ="<samp>" + "<p class='text-muted'>*You wake up in a strange mystical place and a tall thin man stands in front of you*</p><br>'What is your name?'" + "</samp>";
     }
     else{
       document.getElementById('response').innerHTML = "<samp>" + "Hello " + name + " and Welcome to Rifthalen <br> You have been summoned here by the God Queen." + "</samp>";
