018b9fc091dd818653b2138b6c8e28de1b4c4919
CoEvolution Laboratories <jenerestain@gmail.com>
2016-02-11 08:01:04 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var start = function(){
-  innerHTML.getElementById('response') = "What is your name?";
+  document.getElementById('response') = "What is your name?";
   var name = getInfo();
-  innerHTML.getElementById('response') = "Hello " + name + " I am Jesus."
+  document.getElementById('response') = "Hello " + name + " I am Jesus."
   // innerHTML.getElementById
 }
 var getInfo = function(){
