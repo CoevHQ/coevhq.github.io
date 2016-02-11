b94312f0ccabb0f126c16e810f2f9e492e5fb5ab
CoEvolution Laboratories <jenerestain@gmail.com>
2016-02-11 21:24:35 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,10 +5,10 @@ var start = function(){
     if(name==""){
       name = getInfo();
       String(name)
-      document.getElementById('response').innerHTML = "What is your name?";
+      document.getElementById('response').innerHTML ="<samp>" + "<p class="text-muted">*You wake up in a strange mystical place and a tall thin man stands in front of you*</p><br>'What is your name?'" + "</samp>";
     }
     else{
-      document.getElementById('response').innerHTML = "Hello " + name + " I am Jesus.";
+      document.getElementById('response').innerHTML = "<samp>" + "Hello " + name + " and Welcome to Rifthalen <br> You have been summoned here by the God Queen." + "</samp>";
       clearInterval(checkAns);
     }
   }, 2000);
