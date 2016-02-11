11ce4ba447bcf5555b94c48684a0f061711fef4f
CoEvolution Laboratories <jenerestain@gmail.com>
2016-02-11 22:15:58 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,12 @@ var start = function(){
   var name = ""
   document.getElementById('response').innerHTML ="<samp>" + "<p class='text-muted'>*You wake up in a strange mystical place and a tall thin man stands in front of you*</p>'Hello and Welcome to Rifthalen.' <br> 'You have been summoned here by the God Queen.'" + "</samp>";
     }
+var getCoins = function(addit){
+    var x = 0
+    var inc = 1
+    if(addit>=0){
+     inc += addit
+    }
+    x += inc
+    document.getElementById('coins').innerHTML = (0+x)
+}
