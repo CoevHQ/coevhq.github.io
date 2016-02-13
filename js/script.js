3455a0ea3e45b27cc981c693adfd9a8a63dcf2c2
CoEvolution Laboratories <jenerestain@gmail.com>
2016-02-13 13:20:18 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 var start = function(){
-  var name = ""
   document.getElementById('response').innerHTML ="<samp>" + "<p class='text-muted'>*You walk by a small old town.*<p>*You see a sign: <b>'Welcome to Ridge--'</b> the rest has rusted off*</p></p>" + "</samp>";
     }
 var getCoins = function(){
     var current = document.getElementById('coins').innerHTML
     var add = (Math.floor((Math.random()*10)+1))
+    var final = 0
     current = Number(current)
     add = Number(add)
+    final = current + add
     var oldValue = document.getElementById('choice3').innerHTML;
 
     document.getElementById('choice3').setAttribute('disabled', true);
@@ -14,6 +15,7 @@ var getCoins = function(){
      setTimeout(function(){
         document.getElementById('choice3').innerHTML = oldValue;
         document.getElementById('choice3').removeAttribute('disabled');
-        document.getElementById('coins').innerHTML = current + add
+        document.getElementById('coins').innerHTML = final
+        document.getElementById('response').innerHTML = "<samp>You searched for scrap and now you have <b>" + final "</b> pieces of scrap.</samp>"
     }, 3000)
 }
