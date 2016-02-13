b138f4a77b3d3424076464eb4c5895d5091a6e16
CoEvolution Laboratories <jenerestain@gmail.com>
2016-02-13 12:55:27 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var start = function(){
   var name = ""
-  document.getElementById('response').innerHTML ="<samp>" + "<p class='text-muted'>*You wake up in a strange mystical place and a tall thin man stands in front of you*</p>'Hello and Welcome to Rifthalen.' <br> 'You have been summoned here by the God Queen.'" + "</samp>";
+  document.getElementById('response').innerHTML ="<samp>" + "<p class='text-muted'>*You walk by a small old town.*<p>*You see a sign: <b>'Welcome to Ridge--'</b> the rest has rusted off*</p>" + "</samp>";
     }
 var getCoins = function(){
     var x = document.getElementById('coins').innerHTML
