c66c5f63a74fa08206a67d21acb92f6cf80c475f
CoEvolution Laboratories <jenerestain@gmail.com>
2016-02-13 13:15:55 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,12 +3,11 @@ var start = function(){
   document.getElementById('response').innerHTML ="<samp>" + "<p class='text-muted'>*You walk by a small old town.*<p>*You see a sign: <b>'Welcome to Ridge--'</b> the rest has rusted off*</p></p>" + "</samp>";
     }
 var getCoins = function(){
-    var x = document.getElementById('coins').innerHTML
-    var y = (Math.floor((Math.random()*10)+1))
-    y=Number(y)
-    x=Number(x)
-    console.log(x)
-    document.getElementById('coins').innerHTML = parseInt(x+y)
+    var current = document.getElementById('coins').innerHTML
+    var add = (Math.floor((Math.random()*10)+1))
+    current = Number(current)
+    add = Number(add)
+    document.getElementById('coins').innerHTML = current + add
     var oldValue = document.getElementById('choice3').innerHTML;
 
     document.getElementById('choice3').setAttribute('disabled', true);
