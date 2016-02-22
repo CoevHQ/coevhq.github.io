0ca9eaf1294152da86c5e4affa26f4092062141e
CoEvolution Laboratories <jenerestain@gmail.com>
2016-02-22 10:26:56 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,4 +18,20 @@ var getCoins = function(){
         document.getElementById('coins').innerHTML = final
         document.getElementById('response').innerHTML = "<samp>You searched for scrap and now you have <b>"+final+"</b> pieces of scrap.</samp>"
     }, 2000)
+var cutWood = function(){
+    var current = document.getElementById('wood').innerHTML
+    var add = (Math.floor((Math.random*()*10)+1))
+    var final = 0
+    current = Number(current)
+    add = Number(add)
+    final = current + add
+    var oldValue = document.getElementById('choice1').innherHTML
+    document.getElementById('choice3').setAttribute('disabled', true);
+    document.getElementById('choice3').innerHTML = 'Cutting';
+     setTimeout(function(){
+        document.getElementById('choice3').innerHTML = oldValue;
+        document.getElementById('choice3').removeAttribute('disabled');
+        document.getElementById('coins').innerHTML = final
+        document.getElementById('response').innerHTML = "<samp>You cut wood and now you have <b>"+final+"</b> logs of wood.</samp>"
+{
 }
