2737cf3b9ba25b6610771aa774b4532e1cee6d1f
CoEvolution Laboratories <jenerestain@gmail.com>
2016-02-24 07:52:54 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -41,6 +41,27 @@ var cutWood = function(){
         document.getElementById('response').innerHTML = "<samp>You cut wood and now you have <b>"+final+"</b> logs of wood.</samp>"
 }, 2000)
 }
+var getFood = function() {
+  var current = document.getElementById('coins').innerHTML
+    var add = (Math.floor((Math.random()*10)+1))
+    var final = 0
+    current = Number(current)
+    add = Number(add)
+    final = current + add
+    var oldValue = document.getElementById('choice3').innerHTML;
+
+    document.getElementById('choice3').setAttribute('disabled', true);
+    document.getElementById('choice3').innerHTML = 'Searching';
+     setTimeout(function(){
+        document.getElementById('choice3').innerHTML = oldValue;
+        document.getElementById('choice3').removeAttribute('disabled');
+        document.getElementById('coins').innerHTML = final
+        var time = document.getElementById('time').innerHTML
+        time = Number(time)
+        document.getElementById('time').innerHTML = ++time
+        document.getElementById('response').innerHTML = "<samp>You searched for scrap and now you have <b>"+final+"</b> pieces of scrap.</samp>"
+    }, 2000)
+}
 var gameTime = function() {
       setInterval(function(){
         var time = document.getElementById('time').innerHTML
