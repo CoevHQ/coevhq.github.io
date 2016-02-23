3b2432c6fc63c9bde1b3dcbfce1e80135b1cf3dd
CoEvolution Laboratories <jenerestain@gmail.com>
2016-02-23 20:23:09 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,7 +9,6 @@ var getCoins = function(){
     add = Number(add)
     final = current + add
     var oldValue = document.getElementById('choice3').innerHTML;
-
     document.getElementById('choice3').setAttribute('disabled', true);
     document.getElementById('choice3').innerHTML = 'Searching';
      setTimeout(function(){
@@ -20,18 +19,6 @@ var getCoins = function(){
         time = Number(time)
         document.getElementById('time').innerHTML = ++time
         document.getElementById('response').innerHTML = "<samp>You searched for scrap and now you have <b>"+final+"</b> pieces of scrap.</samp>"
-          if (ran<rak){
-            document.getElementById('response').innerHTML = "<samp>Oh no a zombie attacked. -</samp>"+lost+" health"
-            health = health - lost
-          }
-        ran = (Math.floor((Math.random()*10)+1))
-          if (ran<6) {
-            document.getElementById('response').innerHTML = "<samp>You found an energy bar</samp>"
-            if(health>6){
-              health = 10
-            }
-            health = health+4
-          }
     }, 2000) }
 var cutWood = function(){
     var current = document.getElementById('wood').innerHTML
@@ -51,18 +38,6 @@ var cutWood = function(){
         time = Number(time)
         document.getElementById('time').innerHTML = ++time
         document.getElementById('response').innerHTML = "<samp>You cut wood and now you have <b>"+final+"</b> logs of wood.</samp>"
-          if (ran<rak){
-            document.getElementById('response').innerHTML = "<samp>Oh no a zombie attacked. -</samp>"+lost+" health"
-            health = health - lost
-          }
-        ran = (Math.floor((Math.random()*10)+1))
-          if (ran<6) {
-            document.getElementById('response').innerHTML = "<samp>You found an energy bar</samp>"
-            if(health>6){
-              health = 10
-            }
-            health = health+4
-          }
 }, 2000)
 }
 var getFood = function() {
@@ -84,35 +59,11 @@ var getFood = function() {
         time = Number(time)
         document.getElementById('time').innerHTML = ++time
         document.getElementById('response').innerHTML = "<samp>You hunted for food and now you have <b>"+final+"</b> pieces of meat.</samp>"
-          if (ran<rak){
-            document.getElementById('response').innerHTML = "<samp>Oh no a zombie attacked. -</samp>"+lost+" health"
-            health = health - lost
-          }
-        ran = (Math.floor((Math.random()*10)+1))
-          if (ran<6) {
-            document.getElementById('response').innerHTML = "<samp>You found an energy bar</samp>"
-            if(health>6){
-              health = 10
-            } 
-            health = health+4
 }
 var gameTime = function() {
       setInterval(function(){
         var time = document.getElementById('time').innerHTML
         time = Number(time)
         document.getElementById('time').innerHTML = ++time
-        var lost = (Math.floor((Math.random()*10)+1)) - armor
-          if (ran<rak){
-            document.getElementById('response').innerHTML = "<samp>Oh no a zombie attacked. -</samp>"+lost+" health"
-            health = health - lost
-          }
-        ran = (Math.floor((Math.random()*10)+1))
-          if (ran<6) {
-            document.getElementById('response').innerHTML = "<samp>You found an energy bar</samp>"
-            if(health>6){
-              health = 10
-            }
-            health = health+4
-          }
       }, 20000);
 }
